e1875a54422cd0d1cf7ac35099659ee4e87c8e35
Boris <boris.raskin@fiverr.com>
2013-02-19 10:39:52 +0200

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scraps::Application.config.secret_token = 'f9bcf6338efb352234b53af6f1621d210046865d6c187fe80476b2a8226ebfbb0063820df4764fa8f91e9f5ca89b16850ae96ca79a237ab9401838cd7bb38cf5'
